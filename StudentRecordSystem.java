ab1862835b47deb1c3ae1a364169ce3165b970e2
Kenneth Balbon <150214994+nolifekenn@users.noreply.github.com>
2024-09-22 14:31:25 +0800

    Add files via upload

---
+++ StudentRecordSystem.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.studentrecordsystem;
+
+/**
+ *
+ * @author Kenneth Balbon
+ */
+public class StudentRecordSystem {
+
+    public static void main(String[] args) {
+        LoginPage log = new LoginPage();
+        log.setVisible(true);
+    }
+}
